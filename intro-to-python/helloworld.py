77fa99590de603e5b058014f007232f201de1848
Michael Berrios <mxbics@rit.edu>
2025-02-21 12:52:11 -0500

    First Day of Python

---
+++ intro-to-python/helloworld.py
@@ -0,0 +1,14 @@
+# print is the key word in python to display data on ther terminal screen
+print('hello world')
+print("what's up doc")
+print('what\'s up doc')
+
+# create variables for your name and age
+name = 'Michael Berrios'
+age = 18
+
+print('My name is', name)
+print('My age is', age)
+print()
+
+print('My name is', name, 'and I am', age, 'years old')
