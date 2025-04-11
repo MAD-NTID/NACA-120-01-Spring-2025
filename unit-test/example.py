ae8f96126cc00f7e54b705fb69fe47b79f8ce37b
Michael Berrios <mxbics@rit.edu>
2025-04-11 13:51:00 -0400

    Completed Unit Test Demo

---
+++ unit-test/example.py
@@ -0,0 +1,6 @@
+# this file is the module
+def sum(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
---
+++ unit-test/test_example.py
@@ -0,0 +1,14 @@
+# import the module
+import example
+
+def test_sum_2_5_is_7():
+    # setup
+    num1 = 2
+    num2 = 5
+    expected_result = 7
+
+    # invoke
+    actual_result = example.sum(num1, num2)
+
+    # analyze
+    assert expected_result == actual_result
